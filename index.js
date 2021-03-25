ad1c4b51a2c96a6242460bcfd392e9d796cc90da
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-03-26 00:26:37 +0600

    create and read of CURD is ready

--- index.js
+++ index.js
@@ -8,30 +8,34 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 client.connect(err => {
-  const collection = client.db("programingdb").collection("codingfun");
-//   const product = {name: "modhu", price:25, quantity: 20};
-//   collection.insertOne(product)
-//   .then(result =>{
-//       console.log('one product added');
-//   })
+  const productCollection = client.db("programingdb").collection("codingfun");
+  app.get('/products', (req, res) => {
+    productCollection.find({}).limit(4)
+    .toArray ((err, documents) => {
+      res.send(documents);
+    })
+  })
 
-//   console.log('database connected');
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productCollection.insertOne(product)
+    .then(result => {
+      console.log('data added successfully');
+      res.send('success')
+    })
+  })
 
-app.post("/addProduct", (req, res) => {
-    const product =req.body;
-    console.log(product);
-})
-
-//   client.close();
+  //   client.close();
 });
 
 
 app.get('/', (req, res) => {
-    // res.send("hello I am working")
-    res.sendFile(__dirname + '/index.html');
-}) 
+  // res.send("hello I am working")
+  res.sendFile(__dirname + '/index.html');
+})
 
 app.listen(3000)
