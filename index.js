0efe7927a1d9acf3aeb27b1324ff2a200f22d164
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-03-26 18:11:17 +0600

    all complete

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('express')
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
 const password = ''
 const uri = "mongodb+srv://iamuser:user626@cluster0.ctgcy.mongodb.net/programingdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,23 +15,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 client.connect(err => {
   const productCollection = client.db("programingdb").collection("codingfun");
+
   app.get('/products', (req, res) => {
-    productCollection.find({}).limit(4)
-    .toArray ((err, documents) => {
-      res.send(documents);
-    })
+    productCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
   })
 
   app.post("/addProduct", (req, res) => {
     const product = req.body;
     productCollection.insertOne(product)
-    .then(result => {
-      console.log('data added successfully');
-      res.send('success')
+      .then(result => {
+        console.log('data added successfully');
+        res.redirect('/');
+      })
+  })
+
+   app.patch('/update/:id', (req, res) => {
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    })
+    .then (result => {
+      res.send(result.modifiedCount > 0)
     })
   })
 
   //   client.close();
+
+  app.delete('/delete/:id', (req, res) => {
+    productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0);
+      })
+  })
 });
 
 
